a1e0919c9b04a656f2d22df9ebd1f6cb78462241
Joao Fonseca <70928490+joao2arj@users.noreply.github.com>
2022-09-11 19:35:28 -0300

    Create l1q4.c

---
+++ first_semester/algorithms_programming/lista1/l1q4.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{
+
+int n1, n2, resto, quociente;
+
+printf("Digite o numero dividendo:\n");
+	scanf("%d", &n1);
+
+printf("Digite o numero divisor:\n");
+	scanf("%d", &n2);
+	
+resto = n1%n2;	
+quociente = n1/n2;
+
+printf("O resto da divisao e: %d\n O quociente da divisao e:%d\n", resto, quociente);
+ 
+}
