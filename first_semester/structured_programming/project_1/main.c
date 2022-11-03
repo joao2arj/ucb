b0ec840d9917edbef091270972ec800d56b58cd7
Joao Fonseca <joao2arj@gmail.com>
2022-11-03 17:52:32 -0300

    exclude structs and classes, remove errors

--- first_semester/structured_programming/project_1/main.c
+++ first_semester/structured_programming/project_1/main.c
@@ -3,34 +3,53 @@
 #include <string.h>
 #include <locale.h>
 
-void cadastro(){
-	int i, j;
-	char nome[5][50], email[5][50], sexo[5][15], endereco[5][100];
-    double altura[5];
-    int vacina[5];
+int main(){
+	
+	setlocale(LC_ALL, "PORTUGUESE"); //Setando os caracteres para o padrão português
+	
+	int id, i, j, opc;
+	char nome[1000][50], email[1000][50], sexo[1000][15], endereco[1000][100];
+    double altura[1000];
+    int vacina[1000];
+	
+	menu:
+    printf("PROJETO 1: VETORES\n\n1. Incluir um usuário\n2. Editar um usuário\n3. Excluir um usuário\n4. Buscar um usuário pelo e-mail\n5. Exibir todos os usuários cadastrados\n6. Fazer backup de todos os usuários cadastrados\n7. Restaurar dados\n\nDigite a opção desejada: ");
+    fflush(stdin);
+    scanf("%d", &opc);
+    system("cls");	
+	
+	switch(opc){
+		case 1: //CADASTRO (JOAO)
     
-    printf("Digite a quantidade de usuários que serão cadastrados: ");
-		fflush(stdin);
-    	scanf("%d", &j);
-    	system("cls");
-    
-    for(i=0;i<j;i++){
+    		printf("Digite a quantidade de usuários que serão cadastrados: ");
+			fflush(stdin);
+    		scanf("%d", &j);
+    		system("cls");
+    		
+    		do{	//VALIDAÇÃO DE QUANTIDADE DE USUÁRIOS A SEREM CADASTRADOS
+				if(j > 1000 || j < 0) {
+	    			printf("AVISO: Por favor, digite uma quantidade válida!\nQuantidade: ");
+	    			fflush(stdin);
+	    			scanf("%d", &j);
+	    			system("cls");
+				}
+			} while(j > 1000 || j < 0);
+    	
+    		for(i=0;i<j;i++){
 
-            //NOME
-            printf("# INCLUIR UM USUÁRIO (USUÁRIO: %d)#\n\n", i+1);
-            printf("Nome: ");
-            fflush(stdin);
-            fgets(nome[i], 50, stdin);
-            system("cls");
+	            //NOME
+	            printf("# INCLUIR UM USUÁRIO (USUÁRIO: %d)#\n\nNome: ", i+1);
+	            fflush(stdin);
+	            fgets(nome[i], 50, stdin);
+	            system("cls");
+	
+	            //E-MAIL
+	            printf("# INCLUIR UM USUÁRIO (USUÁRIO: %d)#\n\nE-mail: ", i+1);
+	            fflush(stdin);
+	            fgets(email[i], 50, stdin);
+	            system("cls");
 
-            //E-MAIL
-            printf("# INCLUIR UM USUÁRIO #\n\n");
-            printf("E-mail: ");
-            fflush(stdin);
-            fgets(email[i], 50, stdin);
-            system("cls");
-
-            //OCORRÊNCIA DE "@" NO E-MAIL
+            //VALIDAÇÃO "@" NO E-MAIL
             do {
 	            if(strchr(email[i], '@') == 0) {
 	                printf("\nAVISO: E-mail inválido!\nDigite novamente: ");
@@ -41,81 +60,63 @@ void cadastro(){
             } while (strchr(email[i], '@') == 0);
             
             //SEXO
-            printf("# INCLUIR UM USUÁRIO #\n\n");
-            printf("Digite uma das opções abaixo:\n-Feminino\n-Masculino\n-Não declarar\n");
-			printf("Sexo: ");
+            printf("# INCLUIR UM USUÁRIO (USUÁRIO: %d) #\n\n", i+1);
+            printf("Digite uma das opções abaixo:\n-Feminino\n-Masculino\n-Não declarar\nSexo: ");
             fflush(stdin);
-            fgets(sexo[i], 3, stdin);
+            scanf("%s", &sexo[i]);
             system("cls");
             
-            //AJUSTAR
             //VALIDAÇÃO DO SEXO
-            if(sexo[i] != "Feminino" || sexo[i] != "Masculino" || sexo[i] != "Não declarar" ) {
-            	printf("Formato de sexo inválido!\n\n");
-            	printf("Digite uma das opções abaixo:\n-Feminino\n-Masculino\n-Não declarar\n");
-				printf("Sexo: ");
-            	fflush(stdin);
-            	fgets(sexo[i], 30, stdin);
-            	system("cls");
-			}
-			
-            
+			do{
+				if(strcmp(sexo[i], "Feminino") != 0 && strcmp(sexo[i], "Masculino") != 0 && strcmp(sexo[i], "Não declarar") != 0) {
+            		printf("AVISO: Formato de sexo inválido!\n\n");
+            		printf("Digite uma das opções abaixo:\n-Feminino\n-Masculino\n-Não declarar\nSexo: ");
+            		fflush(stdin);
+            		scanf("%s", &sexo[i]); //NECESSÁRIO USAR SCANF AO INVÉS DE FGETS (CONFLITO NO STRCMP)
+            		system("cls");
+				}
+			} while(strcmp(sexo[i], "Feminino") != 0 && strcmp(sexo[i], "Masculino") != 0 && strcmp(sexo[i], "Não declarar") != 0);
+
             //ENDEREÇO
-            printf("# INCLUIR UM USUÁRIO #\n\n");
-            printf("Endereço:\n");
+            printf("# INCLUIR UM USUÁRIO (USUÁRIO: %d)#\n\nEndereço: ", i+1);
             fflush(stdin);
-            fgets(endereco[i], 20, stdin);
+            fgets(endereco[i], 100, stdin);
             system("cls");
             
             //ALTURA
-            printf("# INCLUIR UM USUÁRIO #\n\n");
-			printf("Altura (entre 1 e 2 metros): ");
+            printf("# INCLUIR UM USUÁRIO (USUÁRIO: %d)#\n\nAltura (entre 1 e 2 metros): ", i+1);
             fflush(stdin);
             scanf("%lf", &altura[i]);
             system("cls");
             
-         	if(altura[i] < 1 || altura[i] > 2) {
-         		printf("AVISO: Altura inválida!\n");
-         		printf("Digite a altura (Entre 1 e 2 metros): ");
-         		fflush(stdin);
-            	scanf("%lf", &altura[i]);
-            	system("cls");
-			 }
+            //VALIDAÇÃO DA ALTURA
+         	do{
+			 	if(altura[i] < 1 || altura[i] > 2) {
+         			printf("AVISO: Altura inválida!\n\nAltura (entre 1 e 2 metros): ");
+         			fflush(stdin);
+            		scanf("%lf", &altura[i]);
+            		system("cls");
+			 	}
+			} while(altura[i] < 1 || altura[i] > 2);
             
             //VACINA
-            printf("# INCLUIR UM USUÁRIO #\n\n");
-            printf("Digite uma das opções abaixo:\n0. Não vacinei\n1. Vacinei\n");
-			printf("Você vacinou? ");
+            printf("# INCLUIR UM USUÁRIO (USUÁRIO: %d)#\n\nVacina:\n", i+1);
+            printf("Digite uma das opções abaixo:\n0. Não vacinei\n1. Vacinei\nVocê vacinou? ");
             fflush(stdin);
             scanf("%d", &vacina[i]);
             system("cls");
             
-            printf("\n# USUÁRIO CADASTRADO COM SUCESSO! #\n\n\n");
-
-    };
-}
-
-void exibir_usuarios(){
-	
-}
-int main(){
-	setlocale(LC_ALL, "PORTUGUESE");
-	
-	int id, i, j, opc;
-	char nome[5][50], email[5][50], sexo[5][15], endereco[5][100];
-    double altura[5];
-    int vacina[5];
-	
-	menu:
-    printf("PROJETO 1: VETORES\n\n1. Incluir um usuário\n2. Editar um usuário\n3. Excluir um usuário\n4. Buscar um usuário pelo e-mail\n5. Exibir todos os usuários cadastrados\n6. Fazer backup de todos os usuários cadastrados\n7. Restaurar dados\n\nDigite a opção desejada: ");
-    fflush(stdin);
-    scanf("%d", &opc);
-    system("cls");	
-	
-	switch(opc){
-		case 1: //CADASTRO (JOAO)
-    		
-    		cadastro();
+            //VALIDAÇÃO DE VACINA
+            do{
+            	if(vacina[i] != 1 && vacina[i] != 0){
+            		printf("AVISO: Reposta inválida!\n\nDigite uma das opções abaixo:\n0. Não vacinei\n1. Vacinei\nVocê vacinou? ");
+            		fflush(stdin);
+            		scanf("%d", &vacina[i]);
+            		system("cls");
+				}
+			} while(vacina[i] != 1 && vacina[i] != 0);
+            
+            printf("\n# USUÁRIO #%d CADASTRADO COM SUCESSO! #\n\n\n", i+1);
     		
     		goto menu;
 			break;
@@ -157,4 +158,4 @@ int main(){
 	}
 	
 	return 0;
-}
+}}
