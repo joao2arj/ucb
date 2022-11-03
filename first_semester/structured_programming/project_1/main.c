391be78858a805a082ea88520582317880953966
Joao Fonseca <joao2arj@gmail.com>
2022-11-03 09:30:50 -0300

    add positions

--- first_semester/structured_programming/project_1/main.c
+++ first_semester/structured_programming/project_1/main.c
@@ -1,91 +1,160 @@
-/* Projeto 01:
-
-Utilizando os conceitos de vetor, implemente um algoritmo que:
-
-    inclua até 1000 usuários;
-    edite um usuário;
-    exclua um usuário;
-    busque um usuário pelo email;
-    imprima todos os usuários cadastrados;
-    faça backup dos usuários cadastrados;
-    faça restauração dos dados;
-
-Dados do usuário:
-
-    Id (int) => preenchido automaticamente por números randômicos
-    Nome completo (string)
-    Email (string) => validação do campo: verificar se o caractere "@" aparece
-    Sexo (string) => validação do campo: aceitar somente as palavras Feminino, Masculino e Não declarar
-    Endereço (string)
-    Altura (double) => validação do campo: aceitar valores entre 1 e 2 m.
-    Vacina (tomou a vacina? boolean)
-
-Duas equipes serão sorteadas para realizar a apresentação do projeto, tendo 15 min para cada apresentação.
-*/
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <locale.h>
 
-struct user {
-    char nome[100], email[100], sexo[3], endereco[100];
-    double altura;
+void cadastro(){
+	int i, j;
+	char nome[5][50], email[5][50], sexo[5][15], endereco[5][100];
+    double altura[5];
+    int vacina[5];
     
-};
+    printf("Digite a quantidade de usuários que serão cadastrados: ");
+		fflush(stdin);
+    	scanf("%d", &j);
+    	system("cls");
+    
+    for(i=0;i<j;i++){
 
-int main() {
+            //NOME
+            printf("# INCLUIR UM USUÁRIO (USUÁRIO: %d)#\n\n", i+1);
+            printf("Nome: ");
+            fflush(stdin);
+            fgets(nome[i], 50, stdin);
+            system("cls");
+
+            //E-MAIL
+            printf("# INCLUIR UM USUÁRIO #\n\n");
+            printf("E-mail: ");
+            fflush(stdin);
+            fgets(email[i], 50, stdin);
+            system("cls");
+
+            //OCORRÊNCIA DE "@" NO E-MAIL
+            do {
+	            if(strchr(email[i], '@') == 0) {
+	                printf("\nAVISO: E-mail inválido!\nDigite novamente: ");
+	                fflush(stdin);
+	                fgets(email[i], 50, stdin);
+	                system("cls");
+	            }
+            } while (strchr(email[i], '@') == 0);
+            
+            //SEXO
+            printf("# INCLUIR UM USUÁRIO #\n\n");
+            printf("Digite uma das opções abaixo:\n-Feminino\n-Masculino\n-Não declarar\n");
+			printf("Sexo: ");
+            fflush(stdin);
+            fgets(sexo[i], 3, stdin);
+            system("cls");
+            
+            //AJUSTAR
+            //VALIDAÇÃO DO SEXO
+            if(sexo[i] != "Feminino" || sexo[i] != "Masculino" || sexo[i] != "Não declarar" ) {
+            	printf("Formato de sexo inválido!\n\n");
+            	printf("Digite uma das opções abaixo:\n-Feminino\n-Masculino\n-Não declarar\n");
+				printf("Sexo: ");
+            	fflush(stdin);
+            	fgets(sexo[i], 30, stdin);
+            	system("cls");
+			}
+			
+            
+            //ENDEREÇO
+            printf("# INCLUIR UM USUÁRIO #\n\n");
+            printf("Endereço:\n");
+            fflush(stdin);
+            fgets(endereco[i], 20, stdin);
+            system("cls");
+            
+            //ALTURA
+            printf("# INCLUIR UM USUÁRIO #\n\n");
+			printf("Altura (entre 1 e 2 metros): ");
+            fflush(stdin);
+            scanf("%lf", &altura[i]);
+            system("cls");
+            
+         	if(altura[i] < 1 || altura[i] > 2) {
+         		printf("AVISO: Altura inválida!\n");
+         		printf("Digite a altura (Entre 1 e 2 metros): ");
+         		fflush(stdin);
+            	scanf("%lf", &altura[i]);
+            	system("cls");
+			 }
+            
+            //VACINA
+            printf("# INCLUIR UM USUÁRIO #\n\n");
+            printf("Digite uma das opções abaixo:\n0. Não vacinei\n1. Vacinei\n");
+			printf("Você vacinou? ");
+            fflush(stdin);
+            scanf("%d", &vacina[i]);
+            system("cls");
+            
+            printf("\n# USUÁRIO CADASTRADO COM SUCESSO! #\n\n\n");
+
+    };
+}
+
+void exibir_usuarios(){
+	
+}
+int main(){
 	setlocale(LC_ALL, "PORTUGUESE");
-    int opc;
-
-    printf("PROJETO 1: VETORES\n\n");
-    printf("1. Incluir um usuário\n2. Editar um usuário\n3. Excluir um usuário\n4. Buscar um usuário pelo e-mail\n5. Exibir todos os usuários cadastrados\n6. Fazer backup de todos os usuários cadastrados\n7. Restaurar dados\n\n");
-    printf("Digite a opção desejada: ");
+	
+	int id, i, j, opc;
+	char nome[5][50], email[5][50], sexo[5][15], endereco[5][100];
+    double altura[5];
+    int vacina[5];
+	
+	menu:
+    printf("PROJETO 1: VETORES\n\n1. Incluir um usuário\n2. Editar um usuário\n3. Excluir um usuário\n4. Buscar um usuário pelo e-mail\n5. Exibir todos os usuários cadastrados\n6. Fazer backup de todos os usuários cadastrados\n7. Restaurar dados\n\nDigite a opção desejada: ");
+    fflush(stdin);
     scanf("%d", &opc);
-    system("cls");
-
-    switch (opc) {
-
-    case 1:
-
-        printf("# INCLUIR USUÁRIO #\n\n");
-
-        break;
-
-    case 2:
-
-        printf("# EDITAR USUÁRIO #\n\n");
-        break;
-
-    case 3:
-
-        printf("# EXCLUIR USUÁRIO #\n\n");
-        break;
-
-    case 4:
-
-        printf("# BUSCAR USUÁRIO PELO E-MAIL #\n\n");
-        break;
-
-    case 5:
-
-        printf("# EXIBIR USUÁRIOS CADASTRADOS #\n\n");
-        break;
-
-    case 6:
-
-        printf("# FAZER BACKUP DE USUÁRIOS #\n\n");
-        break;
-
-    case 7:
-
-        printf("# RESTAURAR DADOS #\n\n");
-        break;
-    
-    default:
-    
-        printf("AVISO: OPÇÃO INVÁLIDA!\n");
-        break;
-    }
-    return 0;
+    system("cls");	
+	
+	switch(opc){
+		case 1: //CADASTRO (JOAO)
+    		
+    		cadastro();
+    		
+    		goto menu;
+			break;
+			
+		case 2: //EDITAR (LUCAS)
+			
+			goto menu;
+			break;
+			
+		case 3: //DELETAR (HENRIQUE)
+		
+			goto menu;
+			break;
+			
+		case 4: //BUSCAR UM USUÁRIO ()
+		
+			goto menu;
+			break;
+			
+		case 5: //EXIBIR TODOS (JOAO)
+		
+			goto menu;
+			break;
+			
+		case 6: //FAZER BACKUP ()
+		
+			goto menu;
+			break;
+			
+		case 7: //RESTAURAR DADOS ()
+		
+			goto menu;
+			break;
+			
+		default:
+			
+			printf("# AVISO: Opção inválida! #\n\n");
+			goto menu;
+	}
+	
+	return 0;
 }
