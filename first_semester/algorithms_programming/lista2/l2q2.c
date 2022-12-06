dd2b1b5f57ba6d7bddc823e82f1e0d0860aa1303
Joao Fonseca <joao2arj@gmail.com>
2022-12-06 10:35:10 -0300

    Delete first_semester/algorithms_programming/lista2 directory

--- first_semester/algorithms_programming/lista2/l2q2.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-
-int main() {
-	printf("Caderno de poesias é um belo lugar.\nTantas coisas lindas que eu gostaria de falar.\nEu falo em forma de versos para todos poderem escutar.\nAgora você já sabe por que os poetas passam os dias escrevendo em seus cadernos de poesias.\n");
-	
-	return 0;
-}
